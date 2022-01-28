3a14e716a04fef02fab57d1ded6eaaac580c7d9f
briancha <briancha98@gmail.com>
2022-01-27 22:16:15 -0500

    updated schema

--- Queries/schema.sql
+++ Queries/schema.sql
@@ -41,8 +41,7 @@ CREATE TABLE dept_emp (
 	from_date DATE NOT NULL,
     to_date DATE NOT NULL,
 	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
-	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-	PRIMARY KEY (emp_no)
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no)
 );
 
 CREATE TABLE titles (
@@ -50,8 +49,8 @@ CREATE TABLE titles (
 	title VARCHAR(40) NOT NULL,
 	from_date DATE NOT NULL,
     to_date DATE NOT NULL,
-	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-    PRIMARY KEY (emp_no)
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no)
 );
 
-SELECT * FROM departments;
+DROP TABLE titles;
+SELECT * FROM titles;
