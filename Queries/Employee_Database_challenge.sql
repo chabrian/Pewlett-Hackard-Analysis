8f19263917c39601c295b8919d3dc32816e7e438
briancha <briancha98@gmail.com>
2022-02-06 12:32:20 -0500

    updated challenge file

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -1,3 +1,4 @@
+-- DELIVERABLE 1
 -- create the retirement titles table 
 SELECT e.emp_no,
 	   e.first_name,
@@ -37,6 +38,28 @@ ORDER BY COUNT(emp_no) DESC;
 
 -- verify expected results
 SELECT * FROM retiring_titles;
+
+-- DELIVERABLE 2
+-- create a mentorship eligibility table
+SELECT DISTINCT ON (e.emp_no) e.emp_no,
+	   e.first_name,
+	   e.last_name,
+	   e.birth_date,
+	   de.from_date,
+	   de.to_date,
+	   tit.title
+INTO mentorship_eligibility
+FROM employees as e
+INNER JOIN dept_emp as de
+ON (e.emp_no = de.emp_no)
+INNER JOIN titles as tit
+ON (e.emp_no = tit.emp_no)
+WHERE (de.to_date = '9999-01-01')
+AND (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY e.emp_no;
+
+-- verify expected results
+SELECT * FROM mentorship_eligibility;
 
 
 
